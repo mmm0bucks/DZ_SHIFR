93fde418a2e216af6a8907bd95f092d14e25c323
Alex-rosh <alex14ross@mail.ru>
2023-12-14 19:45:49 +0300

    1

---
+++ Roshchektaev_AA-22-08_DZ/Roshchektaev_AA-22-08_DZ.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int x = 1;
+	int y = 1;
+	int i;
+	int n = 6;
+
+	for (i = 2; i < n; i++) {
+		y = x + y;
+		x = y - x;
+	}
+
+	cout << "Number of Fibonachi: " << y;
+}
